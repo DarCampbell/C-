eae7cdc5c0e96e3e9acf39de1c007fba7a4af0c4
Darlisha Campbell <40171398+DarCampbell@users.noreply.github.com>
2022-03-29 16:18:16 -0400

    Add files via upload
    not complete

---
+++ countoddeven.cpp
@@ -0,0 +1,29 @@
+/*
+Write a program that inputs four numbers separated by spaces.  
+The program should count how many odd and even numbers there are. */
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int num1, num2, num3, num4, totalOdd = 0 , totalEven = 0, i;
+    int resultNum1, resultNum2,resultNum3,resultNum4;
+
+    cout << "Please enter 4 positive integers, seperated by a space: ";
+    cin >> num1>>num2>>num3>>num4;
+    
+    resultNum1 = num1 % 2;
+    resultNum2 = num2 % 2;
+    resultNum3 = num3 % 2;
+    resultNum4 = num4 % 2;
+
+    if( i =0; i<4; i++)
+    {
+        
+        
+    }
+    
+}
